99ec3957d40f2554c654bdff04a126052546c15f
mhsohail <sohailx2x@yahoo.com>
2015-07-23 08:44:07 +0500

    tz8768756

--- eQuiz/Controllers/QuestionsController.cs
+++ eQuiz/Controllers/QuestionsController.cs
@@ -40,7 +40,7 @@ namespace eQuiz.Controllers
             DateTime ESTDateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, ESTTimeZone);
             var TimeDiff = QuizStartTime.Subtract(ESTDateTime);
 
-            return QuizStartTime + " - " + ESTDateTime + " - " + TimeDiff;
+            return DateTime.Now + " - " + ESTDateTime + " - " + DateTime.Now.Subtract(ESTDateTime);
             //return local + " - " + universal + " - " + DateTime.UtcNow.ToLocalTime();
             
             //TimeZone.CurrentTimeZone.ToLocalTime()
